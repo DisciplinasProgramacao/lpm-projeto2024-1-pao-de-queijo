48d3c7bc0357fac08b21d5b31a369b5c2033c992
Raquel Calazans <raquel.calazans@localiza.com>
2024-04-07 23:16:56 -0300

    Classe Cliente e início da classe App

--- codigo/pao-de-queijo/src/App.java
+++ codigo/pao-de-queijo/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo ao Restaurante Pão de Queijo!");
+        System.out.println("Já possui cadastro?");
+        System.out.println("1- Sim");
+        System.out.println("2- Não");
+        String resposta = scanner.nextInt();
+
+        if (resposta == 1) {
+            System.out.println("Qual o número do seu documento?");
+            Long documento = scanner.nextLong();
+            Cliente cliente = buscarCliente(documento);
+
+            if (cliente.isNullOrEmpty) {
+                
+            }
+        }
+
+        scanner.close;
     }
 }
---
+++ codigo/pao-de-queijo/src/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+    private String nome;
+    private String documento;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+}
