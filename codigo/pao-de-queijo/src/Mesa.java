8b9cef26f8cb578b210b6789e4c40b1c174d50f0
GustavoMarizA <gustavo.mariz.andrade@gmail.com>
2024-04-10 21:33:48 -0300

    Adiciona get set

--- codigo/pao-de-queijo/src/Mesa.java
+++ codigo/pao-de-queijo/src/Mesa.java
@@ -5,11 +5,39 @@ public class Mesa{
     public int numero;
     private int capacidade;
     private boolean disponivel;
-
+    
+    
+    
     /**
      * Construtor da classe Mesa.
      * @param numero Numero da mesa
      * @param capacidade Capacidade da mesa
      * @param disponivel Mostra se a mesa esta disponivel (diponivel = true) ou não (disponivel = false)
-    */
+     * 
+     */
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public int getCapacidade() {
+        return this.capacidade;
+    }
+    
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    public boolean isDisponivel() {
+        return this.disponivel;
+    }
+    
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
 }
