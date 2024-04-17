505951b5f86971d637dd1a5fa08e13bf43003504
Raquel Calazans <raquel.calazans@localiza.com>
2024-04-17 20:42:06 -0300

    Iterando App

--- codigo/pao-de-queijo/src/App.java
+++ codigo/pao-de-queijo/src/App.java
@@ -1,39 +1,80 @@
 import java.util.Scanner;
 
 public class App {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner;
     static Restaurante restaurante = new Restaurante("Pão de Queijo");
 
-    public static void main(String[] args) throws Exception {
+    static void cabecalho() {
+        System.out.println("========================================");
         System.out.println("Bem vindo ao Restaurante Pão de Queijo!");
-        System.out.println("Já possui cadastro?");
-        System.out.println("1- Sim");
-        System.out.println("2- Não");
-        int resposta = Integer.parseInt(scanner.nextLine());
+        System.out.println("========================================");
+    }
 
-        switch (resposta) {
-            case 1:
-                System.out.println("Qual o número do seu documento?");
-                Long documento = Long.parseLong(scanner.nextLine());
-                Cliente cliente = restaurante.buscarCliente(documento);
+    static int menu() {
+        int opcao;
+        cabecalho();
 
-                if (cliente.isNullOrEmpty) {
-                    
-                }
-                    
-                break;
+        System.out.println("1 - Abrir Requisicao");
+        System.out.println("0 - Sair");
+        System.out.print("Digite sua opção: ");
+        opcao = Integer.parseInt(scanner.nextLine());
+
+        return opcao;
+    }
+
+    static Cliente buscarCliente() {
+        Cliente cliente;
+        cabecalho();
+
+        System.out.print("Qual o seu documento (somente números)? ");
+        long documento = Long.parseLong(scanner.nextLine());
+        cliente = restaurante.buscarCliente(documento);
+
+        return cliente;
+    }
+
+    static Cliente cadastrarNovoCliente() {
+        Cliente cliente;
+
+        System.out.print("Qual o seu nome? ");
+        String nome = scanner.nextLine();
+        System.out.print("Qual o seu documento (somente números)? ");
+        long documento = Long.parseLong(scanner.nextLine());
+        cliente = new Cliente(nome, documento);
+
+        return cliente;
+    }
+
+    static Requisicao criarRequisicao(Cliente cliente) {
+        Requisicao requisicao;
+
+        System.out.print("Mesa para quantas pessoas? ");
+        int quantPessoas = Integer.parseInt(scanner.nextLine());
+        requisicao = requisicao.criarRequisicao(cliente, quantPessoas);
+
+        return requisicao;
+    }
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        Requisicao requisicaoAtual;
+        Cliente cliente;
+        int opcao;
         
-            case 2: 
-                System.out.println("Qual o seu nome?");
-                String nomeCliente = scanner.nextLine();
-                System.out.println("Qual o número do seu documento?");
-                Long documentoCliente = scanner.nextLong();
-                Cliente novoCliente = new Cliente(nomeCliente, documentoCliente);
-                break;
-            default:
-                break;
-        }
-
+        do {
+            opcao = menu();
+            switch(opcao) {
+                case 1:
+                    cliente = buscarCliente();
+                    if (cliente == null) {
+                        cliente = cadastrarNovoCliente();
+                    }
+                    requisicaoAtual = criarRequisicao(cliente);
+                    validarMesa(requisicaoAtual);
+            }
+            
+        } while (opcao != 0);
+        System.out.println("Agradeçemos a preferência, volte sempre.");
         scanner.close();
     }
 }
